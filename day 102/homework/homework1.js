bf3b01c8124a6513ed8b21e17345df9a24c4c832
iakobdoghonadze <iakolaiki@gmail.com>
2026-02-07 17:59:40 +0400

    commit

--- day 102/homework/homework1.js
+++ day 102/homework/homework1.js
@@ -1,1 +1,20 @@
 //კომენტარის სახით ახსენით რა განსხვავებაა for of / for in შორის
+
+// for...in - gamoiyeneba obieqtis an masivis index da key misawvdomlad 
+// magalitad
+// let list = ['a', 'b', 'c']
+// for (let i in list) {
+//   console.log(i)
+// }
+// gamoitans 0, 1, 2 (index) romelebic miviget for in ciklis meshveobit
+// for in - miwvda masivs da yovel iteraciaze gamoitana index
+// 
+
+// for...of - gamoiyeneba obieqtis an masivis value(mnishvnelobebis) misawvdomlad
+// magalitad
+// let list = ['a', 'b', 'c']
+// for (let value of list){
+//   console.log(value)
+// }
+// gamoitans 'a', 'b', 'c' (value) romelebic miviget for of ciklis meshveobit,
+// for of - miwvda masivs da yovel iteraciaze gamoitana value(mnishvneloba) 
