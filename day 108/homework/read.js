bc9424efd57599036bd842e9a9983d3e5dd9b1c6
iakobdoghonadze <iakolaiki@gmail.com>
2026-04-16 03:44:18 +0400

    commit

--- day 108/homework/read.js
+++
@@ -1,4 +0,0 @@
-//everything past and including this is still being worked on as i have no clear understanding of the material needed to complete the said homework(homework 1 and up)
-
-//and yes there is only day 107 homework done the rest idfk. ❤
-
